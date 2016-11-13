2cb14f2ae25bc00484224312b9fb80b21de000d0
John Martin <john.f.martin@duke.edu>
2016-11-13 13:16:28 -0500

    Added frontend interfaces

---
+++ src/design/gameeditor/IGameEditorFrontend.java
@@ -0,0 +1,20 @@
+package design.gameeditor;
+
+public interface IGameEditorFrontend {
+	
+	// Method to set level type: either distinct finite levels or infinite levels
+	public void setLevelType(String levelType);
+	
+	//Method to set an interaction between two objects
+	public void setInteractionBehaviour(String object1, String object2, String behaviourProperty);
+	
+	// Method to set an XML property on creation of a new object/rule/setting
+	public void setProperty(String propName, String propValue);
+	
+	// Method to retrieve XML properties to enable loading a game design from file
+	public void getProperty(String propName);
+	
+	// Method to reset game design, clearing previously set XML properties
+	public void reset();
+	
+}
