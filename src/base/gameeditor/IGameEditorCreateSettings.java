d207c2353e03d80af65bb56b001e3dca21ee4a5c
Ray Song <ys101@duke.edu>
2016-11-13 20:08:54 -0500

    Added author tags and descriptions for Game Editor use case

--- src/base/gameeditor/IGameEditorCreateSettings.java
+++ src/base/gameeditor/IGameEditorCreateSettings.java
@@ -4,6 +4,11 @@ import com.sun.prism.Image;
 
 import javafx.scene.media.Media;
 
+/**
+ * 
+ * @author @author Ray Song(ys101)
+ *
+ */
 public interface IGameEditorCreateSettings {
 	void setBGM(Media musicfile);
 	void setBackgroundImage(Image image);
