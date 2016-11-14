bba8a8bc7cd1c0a4642b6084134824b0c0a7995f
Eric Song <ess42@duke.edu>
2016-11-14 12:07:45 -0500

    added Parser and GameParser for structuring parsing of xml file for game engine

--- src/base/gameengine/controller/IGameEngineController.java
+++ src/base/gameengine/controller/IGameEngineController.java
@@ -3,12 +3,8 @@ package base.gameengine.controller;
 import java.util.Observable;
 import java.util.Observer;
 
-public interface IGameEngineController {
+public interface IGameEngineController extends Observer{
 
     public void startGame(String fileName);
-
-    public void update (Observable o, Object arg);
-
-
 
 }
--- src/general/MainController.java
+++ src/general/MainController.java
@@ -1,11 +1,19 @@
 package general;
 
+import base.gameengine.controller.GameEngineController;
+import base.gameengine.controller.GameParser;
 import javafx.stage.Stage;
 
+/**
+ * @author ericsong
+ *
+ */
 public class MainController {
 	
+	GameEngineController gameEngineController;
+	
 	public MainController(Stage stage){
-		
+		gameEngineController = new GameEngineController(new GameParser());
 	}
 
 }
