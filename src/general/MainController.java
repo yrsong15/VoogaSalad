bf6372aa7cd3d772f79867b0668ac707ec9afe51
DELIA LI <DL202@duke.edu>
2016-11-17 12:37:19 -0500

    fixed file searching

--- src/general/MainController.java
+++ src/general/MainController.java
@@ -10,16 +10,17 @@ import javafx.stage.Stage;
 
 public class MainController {
    public static final String STYLESHEET = "default.css";
-     public static final String CSS_RESOURCE_PACKAGE = "resources.properties";
+     public static final String CSS_RESOURCE_PACKAGE = "resources";
+    public static final String FILE_SEPARATOR = "/";
 
     public MainController(Stage stage){
         Scene scene = new Scene(new SplashScreen(stage).setUpWindow());
         //GameEditorView myView = new GameEditorView();
         //Scene scene = new Scene(myView.createRoot(),GameEditorView.SCENE_WIDTH,GameEditorView.SCENE_HEIGHT);
-        scene.getStylesheets().add(CSS_RESOURCE_PACKAGE + File.separator + STYLESHEET);
+        scene.getStylesheets().add(CSS_RESOURCE_PACKAGE + FILE_SEPARATOR + STYLESHEET);
         stage.setScene(scene);
         stage.setTitle("VoogaSalad");
-        stage.show(); 	
+        stage.show();
     }
 
 }
