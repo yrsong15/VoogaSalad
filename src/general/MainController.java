61eff8a37f0735f2bbc627ba08e18192e0b897aa
DELIA LI <DL202@duke.edu>
2016-11-18 14:38:46 -0500

    added galleryView bindings

--- src/general/MainController.java
+++ src/general/MainController.java
@@ -6,25 +6,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-
 public class MainController {
-   public static final String STYLESHEET = "default.css";
+    public static final String STYLESHEET = "default.css";
 
-   public static final String CSS_RESOURCE_PACKAGE = "resources";
+    public static final String CSS_RESOURCE_PACKAGE = "resources";
     /**
      * IDEALLY, WE GET THE css FOLDER WORKING AND PUT ALL STYLESHEETS THERE.
      * FOR NOW, resources WILL HAVE TO BE TEMPORARY
      */
-   public static final String FILE_SEPARATOR = "/";
-   private static final String GALLERY_STAGE_TITLE = "Game Gallery"; //TODO: Replace this with a resource file
-   private Stage myGalleryStage;
-   private Gallery myGallery;
+    public static final String FILE_SEPARATOR = "/";
+    private static final String GALLERY_STAGE_TITLE = "Game Gallery"; //TODO: Replace this with a resource file
+    private Stage myGalleryStage;
+    private Gallery myGallery;
     private GalleryView myGalleryView;
     private Stage myGameEditorStage;
     private GameEditorView myGameEditorView;
 
-   public MainController(Stage stage){
-        Scene scene = new Scene(new SplashScreen(stage,this).setUpWindow());
+    public MainController(Stage stage) {
+        Scene scene = new Scene(new SplashScreen(stage, this).setUpWindow());
         //GameEditorView myView = new GameEditorView();
         //Scene scene = new Scene(myView.createRoot(),GameEditorView.SCENE_WIDTH,GameEditorView.SCENE_HEIGHT);
         scene.getStylesheets().add(CSS_RESOURCE_PACKAGE + FILE_SEPARATOR + STYLESHEET);
@@ -41,16 +40,16 @@ public class MainController {
         myGalleryStage.setTitle(GALLERY_STAGE_TITLE);
         myGalleryStage.show();
     }
-    
+
     private void initializeGallery() {
- 	   this.myGallery = new Gallery();
- 	   this.myGalleryStage = new Stage();
+        this.myGallery = new Gallery();
+        this.myGalleryStage = new Stage();
     }
 
-    public void presentEditor(){
+    public void presentEditor() {
         myGameEditorStage = new Stage();
         myGameEditorView = new GameEditorView();
-        Scene scene = new Scene(myGameEditorView.createRoot(),GameEditorView.SCENE_WIDTH,GameEditorView.SCENE_HEIGHT);
+        Scene scene = new Scene(myGameEditorView.createRoot(), GameEditorView.SCENE_WIDTH, GameEditorView.SCENE_HEIGHT);
         myGameEditorStage.setScene(scene);
         myGameEditorStage.show();
     }
