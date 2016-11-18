5e214382a4ba599c24ecde39a79557f0f94419bb
Eric Song <ess42@duke.edu>
2016-11-17 20:54:23 -0500

    created gameobject model class and rulebook classes

---
+++ src/objects/MainCharacter.java
@@ -0,0 +1,11 @@
+package objects;
+
+import java.util.Map;
+
+public class MainCharacter extends GameObject {
+
+	public MainCharacter(double xPosition, double yPosition, String fileName, Map<String, String> properties) {
+		super(xPosition, yPosition, fileName, properties);
+	}
+
+}
