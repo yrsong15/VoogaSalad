39f4a927bd699daf0b023c62a8897ae82d974f80
Brian Zhou <blz4@duke.edu>
2016-11-19 13:08:11 -0500

    Added basic scrolling stuff, going to finish later

--- src/gameengine/view/GameEngineUI.java
+++ src/gameengine/view/GameEngineUI.java
@@ -3,6 +3,7 @@
  */
 package gameengine.view;
 
+import gameengine.controller.ScrollerController;
 import gameengine.view.interfaces.IGameEngineUI;
 import gameengine.view.interfaces.IGameScreen;
 import gameengine.view.interfaces.IToolbar;
@@ -20,9 +21,19 @@ public class GameEngineUI implements IGameEngineUI {
 	public static final double myAppHeight = 775;
 
 	private Scene myScene;
+	private ScrollerController scrollerController;
 	
 	public GameEngineUI() {
 		myScene = new Scene(makeRoot(), myAppWidth, myAppHeight);
+		
+		//TODO: Instantiate the proper ScrollerController depending on game type, right now ScrollerController is abstract
+		// All of the instantiable scrollercontrollers are in gameengine.controller package
+		scrollerController = new ScrollerController();
+		scrollerController.setScene(myScene);
+	}
+	
+	public ScrollerController getScrollerController(){
+		return scrollerController;
 	}
 
 	public Scene getScene() {
