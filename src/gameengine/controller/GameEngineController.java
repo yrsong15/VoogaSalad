ea5f53f49ff9a5c423f31c58694c3cad6530bf2d
Chalena <cm296@duke.edu>
2016-11-18 19:28:51 -0500

    small change

--- src/gameengine/controller/GameEngineController.java
+++ src/gameengine/controller/GameEngineController.java
@@ -35,8 +35,7 @@ public class GameEngineController extends Observable{
 	 * Applies gravity and scrolls, checks for collisions
 	 */
 	public void loopGame(){
-		Game mainGame = null;
-		Level currLevel = mainGame.getCurrentLevel();
+		Level currLevel = currentGame.getCurrentLevel();
 		collisionChecker.checkCollisions(currLevel.getMainCharacter(), currLevel.getGameObjects());
 		//TO-DO: apply movement and scroll screen
 	}
