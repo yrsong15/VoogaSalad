c1be77b305612cee335fdebccea3ee835bbc3cfd
Ray Song <ys101@duke.edu>
2016-11-21 18:54:32 -0500

    Updated GameEditorController with Map, GObject, and Level Manager

--- src/gameeditor/controller/LevelManager.java
+++ src/gameeditor/controller/LevelManager.java
@@ -3,6 +3,11 @@ package gameeditor.controller;
 import gameeditor.controller.interfaces.ILevelManager;
 import objects.Level;
 
+/**
+ * This is an intermediate controller that manages levels of the Game.
+ * @author Ray Song(ys101)
+ *
+ */
 public class LevelManager implements ILevelManager{
 	
 	private Level myLevel;
@@ -15,6 +20,16 @@ public class LevelManager implements ILevelManager{
 
 	public Level getLevel() {
 		return myLevel;
+	}
+
+	@Override
+	public void addWinConditions(String type, String action) {
+		myLevel.addWinCondition(type, action);
+	}
+
+	@Override
+	public void addLoseConditions(String type, String action) {
+		myLevel.addLoseCondition(type, action);
 	}
     
 }
