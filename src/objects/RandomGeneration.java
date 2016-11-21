7ac5cbc91826800b17cc84eba9c94bfadd06a4ba
Soravit Sophastienphong <ss833@duke.edu>
2016-11-21 17:01:21 -0500

    Added random generation class

---
+++ src/objects/RandomGeneration.java
@@ -0,0 +1,26 @@
+package objects;
+
+import java.util.Random;
+
+/**
+ * Created by Soravit on 11/21/2016.
+ */
+public class RandomGeneration {
+
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+    private int minSpacing;
+    private int maxSpacing;
+    
+    private RandomGeneration(int minX, int maxX, int minY, int maxY, int minSpacing, int maxSpacing){
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        this.minSpacing = minSpacing;
+        this.maxSpacing = maxSpacing;
+
+    }
+}
