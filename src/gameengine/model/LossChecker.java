4ed66f62be74099fcf2954bf6af70af02b1cd430
Chalena <cm296@duke.edu>
2016-11-21 20:10:57 -0500

    implemented win/loss checkers

--- src/gameengine/model/LossChecker.java
+++ src/gameengine/model/LossChecker.java
@@ -2,12 +2,16 @@ package gameengine.model;
 
 import java.util.Map;
 
+import gameengine.controller.RuleActionHandler;
+
 public class LossChecker {
 	
 	
-	public void checkLossConditions(Map<String, String> lossConditions){
-		
+	public void checkLossConditions(RuleActionHandler handler, Map<String, String> lossConditions, Map<String, String> gameConditions){
+		for (String condition : lossConditions.keySet()){
+			if (lossConditions.get(condition)==gameConditions.get(condition)){
+				handler.endGame();
+			}
+		}
 	}
-	
-
 }
--- src/gameengine/model/WinChecker.java
+++ src/gameengine/model/WinChecker.java
@@ -2,10 +2,17 @@ package gameengine.model;
 
 import java.util.Map;
 
+import gameengine.controller.RuleActionHandler;
+
 public class WinChecker {
 	
 	
-	public void checkWinConditions(Map<String, String> winConditions){
+	public void checkWinConditions(RuleActionHandler handler, Map<String, String> winConditions, Map<String, String> gameConditions){
+		for (String condition : winConditions.keySet()){
+			if (winConditions.get(condition)==gameConditions.get(condition)){
+				handler.endGame();
+			}
+		}
 		
 		
 	}
