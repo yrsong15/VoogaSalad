1e6423d73159c6d7c478585641d171197b5726f2
Eric Song <ess42@duke.edu>
2016-11-22 00:59:26 -0500

    fixed errors

--- src/gameengine/controller/GameEngineTest.java
+++ src/gameengine/controller/GameEngineTest.java
@@ -17,24 +17,24 @@ public class GameEngineTest {
 
     
 
-    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException, NoSuchMethodException, ClassNotFoundException {
-    	
-    	XStream mySerializer = new XStream(new DomDriver());
-        Map<String, String> map = new HashMap<String, String>();
-    	GameEngineController gameEngineController = new GameEngineController();
-    	Game game = new Game("flappy bird");
-        map.put("collidable", "die");
-        GameObject go = new GameObject(1, 2, 50, 50, "hi", map);
-        go.setProperty("removeobject", "doesn't matter what you put here (remove object doesn't care)");
-        go.setProperty("damage", "50");
-        Level level = new Level(1);
-        level.addGameObject(go);
-        level.addWinCondition("score", 10);
-        level.addLoseCondition("time", 30);
-        game.addLevel(level);
-        game.setCurrentLevel(level);
-        String s = mySerializer.toXML(game);
-        gameEngineController.setCurrentXML(s);
-        gameEngineController.startGame();
-    }
+//    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException, NoSuchMethodException, ClassNotFoundException {
+//    	
+//    	XStream mySerializer = new XStream(new DomDriver());
+//        Map<String, String> map = new HashMap<String, String>();
+//    	GameEngineController gameEngineController = new GameEngineController();
+//    	Game game = new Game("flappy bird");
+//        map.put("collidable", "die");
+//        GameObject go = new GameObject(1, 2, 50, 50, "hi", map);
+//        go.setProperty("removeobject", "doesn't matter what you put here (remove object doesn't care)");
+//        go.setProperty("damage", "50");
+//        Level level = new Level(1);
+//        level.addGameObject(go);
+//        level.addWinCondition("score", 10);
+//        level.addLoseCondition("time", 30);
+//        game.addLevel(level);
+//        game.setCurrentLevel(level);a
+//        String s = mySerializer.toXML(game);
+//        gameEngineController.setCurrentXML(s);
+//        gameEngineController.startGame();
+//    }
 }
