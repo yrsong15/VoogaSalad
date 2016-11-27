1d10688ec06acf0f933fab4537b6135160136db6
Pratiksha Sharma <ps179@duke.edu>
2016-11-26 19:54:40 -0500

    Fixed issues with creating different levels

--- src/gameeditor/view/EditorLevels.java
+++ src/gameeditor/view/EditorLevels.java
@@ -12,14 +12,19 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+/**
+ * 
+ * @author pratikshasharma
+ *
+ */
 
 public class EditorLevels {
-
     public static final double ADD_LEVELS_WIDTH = 400;
     public static final double ADD_LEVELS_HEIGHT=350;
     private VBox myVBox;
@@ -31,7 +36,7 @@ public class EditorLevels {
         Group root = new Group();
         myVBox = new VBox(20);
         myLevels = new ArrayList<Button>();
-        String userDirectoryString = "file:" + System.getProperty("user.dir") + "/images/Background/bubbles.jpg";
+        String userDirectoryString = "file:" + System.getProperty("user.dir") + "/images/Background/bg2.jpg";
         ImageView background = new ImageView(new Image(userDirectoryString));
         background.setFitWidth(SplashScreen.SPLASH_WIDTH);
         background.setFitHeight(SplashScreen.SPLASH_HEIGHT);
@@ -39,21 +44,22 @@ public class EditorLevels {
         root.getChildren().add(background);
 
         ScrollPane myPane = new ScrollPane();
+        
         myPane.setMaxSize(ADD_LEVELS_WIDTH,ADD_LEVELS_HEIGHT);
-
-        myVBox.setPadding(new Insets(10,20,20,20));
-
         myPane.setPrefSize(ADD_LEVELS_WIDTH, ADD_LEVELS_HEIGHT);
         myPane.setStyle("-fx-border-color: black; -fx-border-width: 2px;"); 
-        //myVBox.setStyle(" -fx-background-color: blue");
+        myPane.setLayoutX(200);
+        myPane.setLayoutY(50);
+        
         addButton();
         myPane.setContent(myVBox);
+        
         root.getChildren().addAll(myPane,newLevelButton);
         return root; 
     }
 
     private void addButton(){
-        ButtonTemplate myButton = new ButtonTemplate("LevelCommand", 400, 450);
+        ButtonTemplate myButton = new ButtonTemplate("LevelCommand", 350, 450);
         newLevelButton = myButton.getButton();
 //        newLevelButton.setTranslateX(400);
 //        newLevelButton.setTranslateY(450);
@@ -63,7 +69,7 @@ public class EditorLevels {
         newLevelIcon.setFitHeight(50);
         newLevelIcon.setFitWidth(50);
         newLevelButton.setGraphic(newLevelIcon);
-
+        newLevelButton.setTooltip(new Tooltip("Click Here to add a Level"));
        newLevelButton.setOnAction(e -> addNewLevel());
         myVBox.getChildren().add(newLevelButton);
     }
@@ -83,24 +89,22 @@ public class EditorLevels {
         level.setGraphic(levelIcon);
         myVBox.getChildren().add(level);
         myLevels.add(level);
-        //addLevelListener();
-    
     }
 
     public void setOnLevelClicked(EventHandler<MouseEvent> handler){
         for(Button l:myLevels){
-            l.setOnMouseClicked(handler);
+            l.setOnAction(e -> updateActiveButtonIdAndHandler(handler,l));
             myActiveButtonId.set(l.getId());
-            System.out.println("Button Clicked: ");
-            System.out.println(" myActiveId : " + myActiveButtonId.get());
-        }
+        }   
+    }
+    
+    private void updateActiveButtonIdAndHandler(EventHandler<MouseEvent> handler, Button b){
+        myActiveButtonId.set(b.getId());
+        b.setOnMouseClicked(handler);
         
     }
     
     public SimpleStringProperty getActiveLevelButtonID(){
         return myActiveButtonId;
     }
-    
-    
-
 }
