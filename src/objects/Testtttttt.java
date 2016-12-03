6585d065b92513f21d43d7123fca9b66c718640d
Ray Song <ys101@duke.edu>
2016-12-02 19:41:46 -0500

    Deleted Test class

--- src/objects/Testtttttt.java
+++
@@ -1,8 +0,0 @@
-package objects;
-
-public class Testtttttt {
-	public static void main(String[] args){
-		ObstacleGameBlock obs = new ObstacleGameBlock(10,0,0,0,"aaa");
-		System.out.println(obs.getXPosition());
-	}
-}
