f21635e8e90002da2f491d8cb641efe3770559ca
Ray Song <ys101@duke.edu>
2016-12-02 19:41:24 -0500

    Added GameBlock classes for map creation

---
+++ src/objects/PlatformGameBlock.java
@@ -0,0 +1,11 @@
+package objects;
+
+public class PlatformGameBlock extends AbstractGameBlock{
+	public PlatformGameBlock(double xPosition, double yPosition, double width, double height, String imageFileName){
+		super(xPosition, yPosition, width, height, imageFileName);
+		west = false;
+		east = false;
+		south = true;
+		north = false;
+	}
+}
---
+++ src/objects/Testtttttt.java
@@ -0,0 +1,8 @@
+package objects;
+
+public class Testtttttt {
+	public static void main(String[] args){
+		ObstacleGameBlock obs = new ObstacleGameBlock(10,0,0,0,"aaa");
+		System.out.println(obs.getXPosition());
+	}
+}
