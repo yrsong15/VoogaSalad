3339724a5ddb92c039949d44474f81621846fd92
Eric Song <ess42@duke.edu>
2016-12-05 01:39:02 -0500

    df

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,48 +14,63 @@ import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
-public class Main {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
-  public static void main(String[] args) {
+@WebServlet(name="hello",urlPatterns={"/hello"})
+public class Main extends HttpServlet {
+	  public void doGet(HttpServletRequest request,
+	                    HttpServletResponse response)
+	      throws ServletException, IOException {
+	    PrintWriter out = response.getWriter();
+	    out.println("Hello World");
+	  }
+	}
 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-    get("/hello", (req, res) -> "Hello World");
-
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
-
-  }
-
-}
+//public class Main {
+//
+//  public static void main(String[] args) {
+//
+//    port(Integer.valueOf(System.getenv("PORT")));
+//    staticFileLocation("/public");
+//
+//    get("/hello", (req, res) -> "Hello World");
+//
+//    get("/", (request, response) -> {
+//            Map<String, Object> attributes = new HashMap<>();
+//            attributes.put("message", "Hello World!");
+//
+//            return new ModelAndView(attributes, "index.ftl");
+//        }, new FreeMarkerEngine());
+//
+//    get("/db", (req, res) -> {
+//      Connection connection = null;
+//      Map<String, Object> attributes = new HashMap<>();
+//      try {
+//        connection = DatabaseUrl.extract().getConnection();
+//
+//        Statement stmt = connection.createStatement();
+//        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//        ArrayList<String> output = new ArrayList<String>();
+//        while (rs.next()) {
+//          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+//        }
+//
+//        attributes.put("results", output);
+//        return new ModelAndView(attributes, "db.ftl");
+//      } catch (Exception e) {
+//        attributes.put("message", "There was an error: " + e);
+//        return new ModelAndView(attributes, "error.ftl");
+//      } finally {
+//        if (connection != null) try{connection.close();} catch(SQLException e){}
+//      }
+//    }, new FreeMarkerEngine());
+//
+//  }
+//
+//}
