2c9ca53a383783d108b91a9a1520c615f9a37a61
John Martin <john.f.martin@duke.edu>
2016-12-05 17:43:56 -0500

    changes

---
+++ src/objects/interfaces/IGame.java
@@ -0,0 +1,18 @@
+package objects.interfaces;
+
+import objects.Level;
+/**
+ * @author pratikshasharma
+ */
+
+public interface IGame {
+    public void addLevel(Level level) ;
+    public void removeLevel(int level);
+    public Level getCurrentLevel();
+    public void setCurrentLevel(Level currentLevel);
+    public void setGameName(String name);
+    public int getNumberOfLevels();
+    public String getGameName();
+    public Level getLevelByIndex(int index);
+    
+}
