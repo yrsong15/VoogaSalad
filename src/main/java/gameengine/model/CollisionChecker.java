ee56f1808300a7f6ebb26fc4585669385f387647
Brian Zhou <blz4@duke.edu>
2016-12-07 20:10:00 -0500

    messed up last merge conflict slightly, fixing error in fixing merge conflicts

--- src/main/java/gameengine/model/CollisionChecker.java
+++ src/main/java/gameengine/model/CollisionChecker.java
@@ -49,6 +49,34 @@ public class CollisionChecker {
 			}
 		}
 	}
+
+	
+	/**
+	 * Passes the projectile list and any object colliding with it to the rulebook
+	 * 
+	 * @param firstObjects
+	 * @param secondObjects
+	 */
+	public void checkCollisions(List<GameObject> firstObjects, List<GameObject> secondObjects){
+        for (int i = 0; i < firstObjects.size(); i++) {
+            for (int j = 0; j < secondObjects.size(); j++) {
+                try {
+                    GameObject firstObject = firstObjects.get(i);
+                    GameObject secondObject = secondObjects.get(j);
+                    if (firstObject != secondObject && collision(firstObject, secondObject)) {
+                        try {
+                            rulebook.applyRules(firstObject, secondObject);
+                        } catch (CollisionRuleNotFoundException e) {
+
+                        }
+                    }
+                } catch (ConcurrentModificationException e) {
+                    checkCollisions(firstObjects, secondObjects);
+                    break;
+                }
+            }
+        }
+    }	
 	
 	//Used in both platform apply rules for collision rules
 	public void manuallyRemoveFromConcurrentCollisionList(GameObject obj) {
