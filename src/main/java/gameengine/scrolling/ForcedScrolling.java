7f62d2a7233f860437d4774e7e8c461efc6b535a
Brian Zhou <brian.zhou@duke.edu>
2016-12-12 01:56:43 -0500

    Update ForcedScrolling.java

--- src/main/java/gameengine/scrolling/ForcedScrolling.java
+++ src/main/java/gameengine/scrolling/ForcedScrolling.java
@@ -22,7 +22,7 @@ public class ForcedScrolling implements Scrolling{
 	
 	public ForcedScrolling(Direction dir, double speed, double width, double height){
 		this.direction = dir;
-		this.scrollingSpeed = 2;
+		this.scrollingSpeed = speed;
 		this.screenWidth = width;
 		this.screenHeight = height;
 	}
