e3c2c1772f7f3e1f77922c100fd19d0f4b2f6641
Ray Song <ys101@duke.edu>
2016-12-13 20:07:45 -0500

    small comment change

--- src/main/java/general/MainController.java
+++ src/main/java/general/MainController.java
@@ -106,8 +106,8 @@ public class MainController {
 
     public void launchEngine(String XMLData) {
         GameExamples gameExamples = new GameExamples();
-   //     XMLData = gameExamples.getMultiplayerDDR();
-        XMLData = gameExamples.getDoodleJumpXML();
+        XMLData = gameExamples.getMultiplayerDDR();
+//        XMLData = gameExamples.getDoodleJumpXML();
       //  XMLData = gameExamples.getScrollingXML();
     //   XMLData = gameExamples.getMarioXML();
         boolean multiplayer = true;
