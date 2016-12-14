d0343fd1c16a0696a61ac319582f883c3e856bd4
Noel Moon <nm142@duke.edu>
2016-12-14 15:09:00 -0500

    widened buttons

--- src/main/java/gameengine/view/Toolbar.java
+++ src/main/java/gameengine/view/Toolbar.java
@@ -19,7 +19,7 @@ import javafx.stage.Stage;
  */
 public class Toolbar implements IToolbar {
 
-    private final int DEFAULT_BUTTON_WIDTH = 75;
+    private final int DEFAULT_BUTTON_WIDTH = 90;
     private ResourceBundle myResources;
     private HBox myToolbar;
 
