493ebb3dabf47fe406ae20a2046a6f63fa5b99f5
Pratiksha Sharma <pratiksha.sharma@duke.edu>
2016-12-19 10:06:11 -0500

    Update DetailFactory.java

--- src/main/java/gameeditor/commanddetails/DetailFactory.java
+++ src/main/java/gameeditor/commanddetails/DetailFactory.java
@@ -6,6 +6,7 @@ import gameeditor.view.interfaces.IDetailPane;
 
 //This entire file is part of my masterpiece.
 //Pratiksha Sharma
+// Even though I did not write this file, I included this file because this factory uses reflection to create an instance of CreateDetail class.
 /**
  * @author John Martin
  *
