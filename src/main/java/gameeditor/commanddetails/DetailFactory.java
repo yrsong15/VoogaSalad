cd5f811a743d1526f8b77989700653b3ff1a394c
Pratiksha Sharma <pratiksha.sharma@duke.edu>
2016-12-19 10:10:39 -0500

    Update DetailFactory.java

--- src/main/java/gameeditor/commanddetails/DetailFactory.java
+++ src/main/java/gameeditor/commanddetails/DetailFactory.java
@@ -25,7 +25,6 @@ public class DetailFactory {
 				detail.init();
 				return detail;
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
 		 GameEditorException exception = new GameEditorException();
 		    exception.showError(e.getMessage());
 		
